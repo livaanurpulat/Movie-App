8b5de67912f68eb3b37bc064741f04e2887bf786
livaanurpulat <livaanurpulat@gmail.com>
2023-10-06 14:54:56 +0300

    Movie App

---
+++ screens/MovieScreen.js
@@ -0,0 +1,163 @@
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+  Image,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { styles, theme } from "../theme";
+import { LinearGradient } from "expo-linear-gradient";
+import Cast from "../components/cast";
+import MovieList from "../components/movieList";
+import Loading from "../components/loading";
+import {
+  fallbackMoviePoster,
+  fetchMovieCredits,
+  fetchMovieDetails,
+  fetchSimilarMovies,
+  image500,
+} from "../api/moviedb";
+
+var { width, height } = Dimensions.get("window");
+const ios = Platform.OS == "ios";
+const topMargin = ios ? "" : "mt-3";
+
+export default function MovieScreen() {
+  let movieName = "Ant-Man and the Wasp: Quantumania";
+  const { params: item } = useRoute();
+  const navigation = useNavigation();
+  const [isFavourite, toggleFavourite] = useState(false);
+  const [cast, setCast] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [loading, setLaoding] = useState(false);
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    setLaoding(true);
+    getMovieDetails(item.id);
+    getMovieCredits(item.id);
+    getSimilarMovies(item.id);
+  }, [item]);
+
+  const getMovieDetails = async (id) => {
+    const data = await fetchMovieDetails(id);
+    if (data) setMovie(data);
+    setLaoding(false);
+  };
+
+  const getMovieCredits = async (id) => {
+    const data = await fetchMovieCredits(id);
+    if (data && data.cast) setCast(data.cast);
+  };
+
+  const getSimilarMovies = async (id) => {
+    const data = await fetchSimilarMovies(id);
+    if (data && data.results) setSimilarMovies(data.results);
+  };
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 20 }}
+      className="flex-1 bg-neutral-900"
+    >
+      {/* back button and movie poster */}
+      <View className="w-full">
+        <SafeAreaView
+          className={
+            "absolute z-20 w-full flex-row justify-between items-center px-4" +
+            topMargin
+          }
+        >
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.background}
+            className="rounded-xl p-1"
+          >
+            <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+            <HeartIcon
+              size="35"
+              color={isFavourite ? theme.background : "white"}
+            />
+          </TouchableOpacity>
+        </SafeAreaView>
+        {loading ? (
+          <Loading />
+        ) : (
+          <View>
+            <Image
+              source={{
+                uri: image500(movie?.poster_path) || fallbackMoviePoster,
+              }}
+              style={{ width, height: height * 0.55 }}
+            />
+
+            {/* gradient */}
+            <LinearGradient
+              colors={["transparent", "rgba(23,23,23,0.8)", "rgba(23,23,23,1)"]}
+              style={{ width, height: height * 0.4 }}
+              start={{ x: 0.5, y: 0 }}
+              end={{ x: 0.5, y: 1 }}
+              className="absolute bottom-0"
+            />
+          </View>
+        )}
+      </View>
+
+      {/* movie details */}
+      <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+        {/* title */}
+        <Text className="text-white text-center text-3xl font-bold tracking-wider">
+          {movie?.title}
+        </Text>
+
+        {/* status, releaseDate, runtime */}
+        {movie?.id ? (
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            {movie?.status} • {movie?.release_date?.split("-")[0]} •{" "}
+            {movie?.runtime} min
+          </Text>
+        ) : null}
+
+        {/* genres */}
+        <View className="flex-row justify-center mx-4 space-x-2">
+          {movie?.genres?.map((genre, index) => {
+            let showDot = index + 1 != movie.genres.length;
+            return (
+              <Text
+                key={index}
+                className="text-neutral-400 font-semibold text-base text-center"
+              >
+                {genre?.name} {showDot ? "•" : null}
+              </Text>
+            );
+          })}
+        </View>
+        {/* description */}
+        <Text className="text-neutral-400 mx-4 tracking-wide mb-4">
+          {movie?.overview}
+        </Text>
+      </View>
+
+      {/* cast */}
+      {cast.length > 0 && <Cast navigation={navigation} cast={cast} />}
+
+      {/* similar movies */}
+      {similarMovies.length > 0 && (
+        <MovieList
+          title="Similar Movies"
+          hideSeeAll={true}
+          data={similarMovies}
+        />
+      )}
+    </ScrollView>
+  );
+}
---
+++ screens/SearchScreen.js
@@ -0,0 +1,109 @@
+import {
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+} from "react-native";
+import React, { useCallback, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { XMarkIcon } from "react-native-heroicons/outline";
+import Loading from "../components/loading";
+import { searchMovies, image185, fallbackMoviePoster } from "../api/moviedb";
+import { debounce } from 'lodash';
+
+const { width, height } = Dimensions.get("window");
+
+export default function SearchScreen() {
+  const navigation = useNavigation();
+  const [results, setResults] = useState([]);
+  const [loading, setLaoding] = useState(false);
+
+  const handleSearch = value=>{
+    if(value&& value.length>2){
+      setLaoding(true);
+      searchMovies({
+          query: value,
+          include_adult:'false',
+          language:'en-US',
+          page:'1'
+      }).then(data=>{
+        setLaoding(false);
+        if(data&&data.results) setResults(data.results);
+      })
+    }else{
+      setLaoding(false);
+      setResults([])
+    }
+  }
+
+  const handleTextDebounce = useCallback(debounce(handleSearch, 400), []);
+
+  return (
+    <SafeAreaView className="bg-neutral-800 flex-1">
+      <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
+        <TextInput
+        onChangeText={handleTextDebounce}
+          placeholder="Search Movie"
+          placeholderTextColor={"lightgray"}
+          className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          className="rounded-full p-3 m-1 bg-neutral-500"
+        >
+          <XMarkIcon size="25" color="white" />
+        </TouchableOpacity>
+      </View>
+      {/* results */}
+      {loading ? (
+        <Loading />
+      ) : results.length > 0 ? (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 15 }}
+          className="space-y-3"
+        >
+          <Text className="text-white font-semibold ml-1">
+            Results({results.length})
+          </Text>
+          <View className="flex-row justify-between flex-wrap">
+            {results.map((item, index) => {
+              return (
+                <TouchableWithoutFeedback
+                  key={index}
+                  onPress={() => navigation.push("Movie", item)}
+                >
+                  <View className="space-y-2 mb-4">
+                    <Image
+                      className="rounded-3xl"
+                      source={{uri: image185(item?.poster_path) || fallbackMoviePoster}}
+                      style={{ width: width * 0.44, height: height * 0.3 }}
+                    />
+                    <Text className="text-neutral-300 ml-1">
+                      {item?.title.length > 22
+                        ? item.title.slice(0, 22) + "..."
+                        : item.title}
+                    </Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              );
+            })}
+          </View>
+        </ScrollView>
+      ) : (
+        // no result
+        <View className="flex-row justify-center">
+          <Image
+            source={require("../assets/images/movieTime.png")}
+            className="h-96 w-96"
+          />
+        </View>
+      )}
+    </SafeAreaView>
+  );
+}
